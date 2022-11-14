3c2bf7ead2aa205c2a1365078bc53159f8c5557a
raz <raz.leibovitch1@gmail.com>
2022-11-14 16:25:19 +0200

    update

--- HSubject.cpp
+++ HSubject.cpp
@@ -1,2 +1,22 @@
 
 #include "HSubject.h"
+
+void HSubject::printS(string ky) {
+
+}
+
+void HSubject::printN(string ky, int N) {
+
+}
+
+void HSubject::print() {
+
+}
+
+void HSubject::startNewTable() {
+
+}
+
+void HSubject::addSubjectAndTitle(string s, string t) {
+
+}
--- HashTable.h
+++ HashTable.h
@@ -1,17 +1,16 @@
 
 #ifndef DATASTRUCTURE_EX2_HASHTABLE_H
 #define DATASTRUCTURE_EX2_HASHTABLE_H
+using namespace std;
 #include <iostream>
 #include <string>
-using namespace std;
 #include "Item.h"
-
+#include <cmath>
 
 template <class T,class K>
 class HashTable
 {
 protected:
-
     int size;
     Item<T,K>* arr;
     bool prime(int n);
@@ -22,18 +21,82 @@ public:
     HashTable(int m=10);
     ~HashTable();
     void add(K& key, T& dat);
-    int remove(K key);
+    void remove(K key);
     int search(K key);
-    T* entryData(K i)
-    {
-        int ind = search(i);
-        if (ind == -1)
-            return NULL;
-        else
-            return &(arr[ind].data);
-    }
+    T* entryData(K i);
     void print();
 };
+
+template<class T, class K>
+bool HashTable<T, K>::prime(int n) {
+
+    for(int i = 2; i <= sqrt(n); i++)
+        if(n % i == 0) return false;
+    return true;
+}
+
+template<class T, class K>
+T *HashTable<T, K>::entryData(K i) {
+    int ind = search(i);
+    if (ind == -1)
+        return NULL;
+    else
+        return &(arr[ind].data);
+}
+
+template<class T, class K>
+HashTable<T, K>::HashTable(int m) {
+    while (!prime(m)) m++;
+    arr = new Item<T,K>[m];
+    size = m;
+}
+
+template<class T, class K>
+HashTable<T, K>::~HashTable() {
+    delete arr;
+}
+
+template<class T, class K>
+int HashTable<T, K>::hash(K key, int i) {
+    return h1(key) + i*h2(key);
+}
+
+template<class T, class K>
+void HashTable<T, K>::add(K &key, T &dat) {
+    int i = search(key);
+    if(i != -1){
+        arr[i].data = dat;
+        arr[i].key = key;
+        arr[i].flag = full;
+    }
+}
+
+template<class T, class K>
+int HashTable<T, K>::search(K key) {
+    for(int i = 0; i < size; i++)
+    {
+        int j = hash(key,i);
+        if(j < size && arr[j].flag == full && arr[j].key == key)
+            return i;
+    }
+    return -1;
+}
+
+template<class T, class K>
+void HashTable<T, K>::remove(K key) {
+    int i = search(key);
+    if(i != -1) arr[i].flag = deleted;
+}
+
+template<class T, class K>
+void HashTable<T, K>::print() {
+    for(int i = 0; i < size; i++)
+    {
+        if(arr[i].flag == full)
+            cout<<arr[i].data<<" ";
+    }
+    cout<<endl;
+}
 
 
 #endif
